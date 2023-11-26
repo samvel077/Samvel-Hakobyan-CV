9f857ceef591579a560cc5ba0b412cfd2165b9bd
samvel077 <samvelhakobyan215@gmail.com>
2023-11-26 18:44:17 +0400

    Add Service Worker

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,10 +1,12 @@
-import { NgModule } from '@angular/core';
+import { NgModule, isDevMode } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { RouterOutlet } from "@angular/router";
 import { APP_BASE_HREF } from "@angular/common";
 
 import { AppComponent } from "@app/app.component";
 import { AppRoutingModule } from "@app/app-routing.module";
+import { ServiceWorkerModule } from '@angular/service-worker';
+import { environment } from "@environments/environment";
 
 @NgModule({
     declarations: [
@@ -13,16 +15,22 @@ import { AppRoutingModule } from "@app/app-routing.module";
     imports: [
         BrowserModule,
         RouterOutlet,
-        AppRoutingModule
+        AppRoutingModule,
+        ServiceWorkerModule.register('ngsw-worker.js', {
+          enabled: !isDevMode(),
+          // Register the ServiceWorker as soon as the application is stable
+          // or after 30 seconds (whichever comes first).
+          registrationStrategy: 'registerWhenStable:30000'
+        })
     ],
     providers: [
         // If you change the "baseHref" for local in angular.json "architect" -> "build" -> "options"
         // you also need to pass this provider for routing
 
-        // {
-        //     provide: APP_BASE_HREF,
-        //     useValue: environment.BASE_HREF
-        // },
+        {
+            provide: APP_BASE_HREF,
+            useValue: environment.BASE_HREF
+        },
     ],
     bootstrap: [AppComponent]
 })
