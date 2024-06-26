d3c451552d129f3b6f4eccf2f8ccf92d997f2812
samvel077 <samvelhakobyan215@gmail.com>
2024-06-26 22:55:28 +0400

    fix preloading

--- src/app/utils/preload-images.ts
+++ src/app/utils/preload-images.ts
@@ -2,6 +2,10 @@ export const preloadImages = (urls: string[]) => {
   urls.forEach(url => {
     const img = new Image();
 
+    img.fetchPriority = 'high';
+
+    img.loading = 'eager';
+
     img.src = url;
 
     img.onload = () => {};
