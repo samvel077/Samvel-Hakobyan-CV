852bd42461617b3735fe2bd6680a336aa2dbaa06
s.hakobyan <s.hakobyan@pin-up.tech>
2024-06-27 11:36:16 +0400

    revert changes

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,6 +1,6 @@
 export const environment = {
   // This for local "baseHref" in angular.json, the baseHref must be between / / this lines
-  // BASE_HREF: '/Samvel-Hakobyan-CV',
+  // BASE_HREF: '/Samvel-Hakobyan-CV/',
 
   BASE_HREF: '',
   BASE_URL: 'http://localhost:4200/',
