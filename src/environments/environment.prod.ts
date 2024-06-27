d3251d0d5b76a636da1c2425557b90eb63d1a969
s.hakobyan <s.hakobyan@pin-up.tech>
2024-06-27 11:30:50 +0400

    fix

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -2,8 +2,8 @@ export const environment = {
   // This is for production "baseHref" in angular.json "architect" -> "configurations" -> "production".
   // If you don't add the "baseHref in angular.json you also can use the cli command --base-href /your-href/
   // the baseHref must be between / / this lines
-  BASE_HREF: '/Samvel-Hakobyan-CV/',
-  BASE_URL: 'https://samvel077.github.io/Samvel-Hakobyan-CV/',
+  BASE_HREF: '/Samvel-Hakobyan-CV',
+  BASE_URL: 'https://samvel077.github.io/Samvel-Hakobyan-CV',
   PATH: '/Samvel-Hakobyan-CV',
   production: true,
 };
