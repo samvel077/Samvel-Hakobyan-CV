2ea4b98c433e84aa4096a20d2a2fc2364ff325c1
s.hakobyan <s.hakobyan@pin-up.tech>
2024-09-23 18:20:10 +0400

    fix Cumulative Layout Shift

--- src/app/pages/main/components/main-info-skeleton/main-info-skeleton.component.ts
+++ src/app/pages/main/components/main-info-skeleton/main-info-skeleton.component.ts
@@ -7,5 +7,5 @@ import { ChangeDetectionStrategy, Component } from '@angular/core';
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class MainInfoSkeletonComponent {
-  loadingTemplates: void[] = Array(8).fill(null);
+  loadingTemplates: void[] = Array(14).fill(null);
 }
